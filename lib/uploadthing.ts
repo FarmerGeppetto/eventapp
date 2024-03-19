36e300534f40bea12d6473ffe3d19799f4401a41
Devalex <devalex.programming@gmail.com>
2024-03-19 15:13:46 +0100

    Fixed uploadthing.ts

--- lib/uploadthing.ts
+++ lib/uploadthing.ts
@@ -1,11 +1,6 @@
-import {
-    generateUploadButton,
-    generateUploadDropzone,
-    generateReactHelpers,
-} from "@uploadthing/react";
+import { generateReactHelpers } from "@uploadthing/react/hooks";
 
 import type { OurFileRouter } from "@/app/api/uploadthing/core";
 
-export const UploadButton = generateUploadButton<OurFileRouter>();
-export const UploadDropzone = generateUploadDropzone<OurFileRouter>();
-export const { useUploadThing, uploadFiles } = generateReactHelpers<OurFileRouter>();
+export const { useUploadThing, uploadFiles } =
+    generateReactHelpers<OurFileRouter>();
