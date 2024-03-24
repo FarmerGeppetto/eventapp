748d036fc5a4a5f1423540a19cc64b5818e8d598
Devalex <devalex.programming@gmail.com>
2024-03-24 11:00:19 +0100

    Changed intro text

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -28,8 +28,8 @@ export default async function Home({ searchParams }: SearchParamProps) {
                             Host, Connect, Celebrate: Your Events, Our Platform!
                         </h1>
                         <p className="p-regular-20 md:p-regular-24">
-                            Book and learn helpful tips from 3,168+ mentors in
-                            world-class companies with our global community.
+                            Embrace the Eventful Journey: Learn, Connect, and
+                            Have Fun at Global Gatherings! Join the Adventure!
                         </p>
                         <Button
                             size="lg"
@@ -55,7 +55,7 @@ export default async function Home({ searchParams }: SearchParamProps) {
                 className="wrapper my-8 flex flex-col gap-8 md:gap-12"
             >
                 <h2 className="h2-bold">
-                    Trust by <br /> Thousands of Events
+                    Powering Connections, Linking Communities through Events
                 </h2>
 
                 <div className="flex w-full flex-col gap-5 md:flex-row">
